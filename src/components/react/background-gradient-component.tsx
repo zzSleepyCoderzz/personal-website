69e939c1866c54a39c8a276db67a3bf3fedada01
zzSleepyCoderzz <timothycgy6@gmail.com>
2025-05-27 17:32:08 +0800

    feat: Using Component for Profile Image

---
+++ src/components/react/background-gradient-component.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BackgroundGradient } from "../ui/background-gradient";
+
+
+export function BackgroundGradientDemo() {
+    return (
+        <div>
+            <BackgroundGradient className="rounded-[22px] max-w-sm bg-zinc-900">
+                <img
+                    src={'/Personal-Website/Picture_Timothy-Chin.jpeg'}
+                    alt="Profile Picture of Timothy Chin"
+                    height="350"
+                    width="350"
+                    className="object-cover rounded-2xl"
+                />
+            </BackgroundGradient>
+        </div>
+    );
+}
